463015e05528f82359dd7e4ae783dec03bf0d789
Eliseo Parodi Almaraz <eparodialmaraz@gmail.com>
2019-11-23 20:17:05 -0300

    Generate csv

--- explore_data.py
+++ explore_data.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import os
 
 def get_retweets(x: str):
     return str(x).startswith("re:")
@@ -30,15 +31,35 @@ df = pd.read_csv('./YInt.csv')
 # Repeated messages
 retweets = df.loc[df["message"].apply(get_retweets)]
 tweets = df.loc[df["message"].apply(get_tweets)]
-duplicated = df.loc[df.duplicated(['message'])]
+
+duplicated = df.loc[df.duplicated(['message'], keep=False)]
 duplicated = duplicated.loc[duplicated['message'].apply(get_tweets)]
+
+not_duplicated = df.drop_duplicates(['message'], keep=False)
 
 duplicated_messages_acounts = duplicated['account'].unique()
 all_accounts = df["account"].unique()
 
-# print(get_hashtags(df))
-print(get_mentions(df))
-# print(get_word_count(df))
-# print(get_word_count(tweets))
-# print(get_word_count(retweets))
-# print(get_word_count(duplicated))
+datasets = [
+    ("total", df),
+    ("tweets", tweets),
+    ("retweets", tweets),
+    ("not_duplicated", not_duplicated)
+]
+
+functions = [
+    ("word_count", get_word_count),
+    ("mentions", get_mentions),
+    ("hashtags", get_hashtags)
+]
+
+if not os.path.exists("./datasets"):
+    os.makedirs("./datasets")
+
+for dataset in datasets:
+    for function in functions:
+        data = function[1](dataset[1])
+        data.to_csv("./datasets/{dataset}_{function}.csv".format(
+            dataset=dataset[0],
+            function=function[0],
+        ))
